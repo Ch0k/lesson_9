7872f70f3b68064d8559109f438cabf4631dbb4c
andreevsergey <andreev,sv89@yandex.ru>
2021-07-02 17:14:52 +0300

    using rubocop and edit presence_validation method

--- atrr_acc.rb
+++ atrr_acc.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
+# Module attribute accessor
 module Attr
-  
-  def my_attr_accessor(*names)
+  def attr_accessor(*names)
     names.each do |name|
       var_name = "@#{name}".to_sym
       history = []
@@ -8,26 +10,27 @@ module Attr
       define_method("#{name}=".to_sym) do |value|
         instance_variable_set(var_name, value)
         history << value
-      end 
-    define_method("#{name}_history") { history }
+      end
+      define_method("#{name}_history") { history }
+    end
   end
 
   def strong_attr_accessor(name, type)
     var_name = "@#{name}".to_sym
-    type = type
     define_method(name) { instance_variable_get(var_name) }
     define_method("#{name}=".to_sym) do |value|
-      raise "Type not true" if value.class != type
+      raise 'Type not true' if value.class != type
+
       instance_variable_set(var_name, value)
     end
   end
-end
 end
 
 class Tran
   extend Attr
 
-  my_attr_accessor :ttt, :aaa
+  attr_accessor :ttt, :aaa
+
   strong_attr_accessor(:vvv, Integer)
   strong_attr_accessor(:sss, String)
 end
--- validation.rb
+++ validation.rb
@@ -1,81 +1,68 @@
-module Validation
 # frozen_string_literal: true
+
+# module for validation
+module Validation
+  # frozen_string_literal: true
 
   def self.included(base)
     base.extend ClassMethods
     base.include InstanceMethods
   end
 
+  # module for craete variable for create methods
   module ClassMethods
     attr_accessor :var_array
+
     def validate(var, type, *params)
       @var_array ||= []
-      @var_array << { var_name: var, type_name: type, params: params}
+      @var_array << { var_name: var, type_name: type, params: params }
     end
-
   end
 
+  # module for validation instances methods
   module InstanceMethods
-
-  def validate!
-    validate_presence
-    true
-  rescue StandardError
-    false
-    #Содержит инстанс-метод validate!, который запускает все проверки (валидации), 
-    #указанные в классе через метод класса validate. В случае ошибки валидации 
-    #выбрасывает исключение с сообщением о том, какая именно валидация не прошла
+    def validate!
+      validate_presence
+      true
+    rescue StandardError
+      false
     end
-    
-  def valid?
-    validate_presence
-    true
-      #Содержит инстанс-метод valid? который возвращает true, если все проверки 
-      #валидации прошли успешно и false, если есть ошибки валидации.
-  end
 
-  def validate_presence
-    @fff = self.class.var_array
-    @fff.each do |var|
-      if var[:type_name] == :presence
-        self.send(var[:type_name],(var[:var_name]))
-      elsif var[:type_name] == :format
-        self.send(var[:type_name],(var[:var_name]),var[:params])
-      else var[:type_name] == :type
-        self.send(var[:type_name],(var[:var_name]),var[:params])
-      end
+    def valid?
+      validate_presence
+      true
     end
-  end
 
-  def presence(name)
-    raise 'Number should be at least 6 symbols' if name.nil?
-  end
+    def validate_presence
+      @fff = self.class.var_array
+      @fff.each { |var| send(var[:type_name], (var[:var_name]), var[:params]) }
+    end
 
-  def type(name, params)
-    raise 'Class has invalid type' if name.class != params[0]
-  end
+    def presence(name, _)
+      raise 'Number should be at least 6 symbols' if name.nil?
+    end
 
-  def format(name, params)
-    raise 'Number has invalid format' if name !~ params[0]
-    #raise 'Number has invalid format' if name !~ /#{Regexp.quote([A-Za-z])}/
-  end
+    def type(name, params)
+      raise 'Class has invalid type' if name.class != params[0]
+    end
 
+    def format(name, params)
+      raise 'Number has invalid format' if name !~ params[0]
+    end
   end
 end
 
-
-
+# Class for Test
 class Test
   include Validation
-  #validate :name, :presence
-  #validate :namesss, :format, /[a-z]{6,}/
- # validate :number, :format, /^[\d\w]{3}-?[\d\w]{2}$/
+  # validate :name, :presence
+  # validate :namesss, :format, /[a-z]{6,}/
+  # validate :number, :format, /^[\d\w]{3}-?[\d\w]{2}$/
   validate :name, :type, Symbol
-  #validate :format, :number, /^[\d\w]{3}-?[\d\w]{2}$/
+  # validate :format, :number, /^[\d\w]{3}-?[\d\w]{2}$/
 
-  def initialize(name)
-  end
-  
- # validate :name, :format
- # validate :names, :type
+  def initialize(name); end
+
+  # validate :name, :format
+  # validate :names, :type
 end
